b91b91bd284588bd77d1c8b26a69b388e951901a
Afonso Bordado <afonso@nortech.ai>
2024-12-12 17:42:44 +0000

    Add explicit alpine version

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 PYTHON_VERSION=3.13
-IMAGE_NAME=arm32v7/python:${PYTHON_VERSION}-alpine
+ALPINE_VERSION=3.21
+IMAGE_NAME=arm32v7/python:${PYTHON_VERSION}-alpine${ALPINE_VERSION}
 
-docker run --rm --platform linux/arm/v7 -v "$(pwd):/work" -w /work ${IMAGE_NAME} /bin/sh -c "tar -cvf python-${PYTHON_VERSION}-armv7-musl.tar.gz -T copyfiles"
+docker run --rm --platform linux/arm/v7 -v "$(pwd):/work" -w /work ${IMAGE_NAME} /bin/sh -c "tar -cvf python-${PYTHON_VERSION}-alpine${ALPINE_VERSION}-armv7-musl.tar.gz -T copyfiles"
