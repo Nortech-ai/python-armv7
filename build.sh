1f3feb0bbbd068369568653545706ff0ae01404b
Afonso Bordado <afonso@nortech.ai>
2024-12-12 17:38:59 +0000

    Explicitly set platform

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 PYTHON_VERSION=3.13
 IMAGE_NAME=arm32v7/python:${PYTHON_VERSION}-alpine
 
-docker run --rm -v "$(pwd):/work" -w /work ${IMAGE_NAME} /bin/sh -c "tar -cvf python-${PYTHON_VERSION}-armv7-musl.tar.gz -T copyfiles"
+docker run --rm --platform linux/arm/v7 -v "$(pwd):/work" -w /work ${IMAGE_NAME} /bin/sh -c "tar -cvf python-${PYTHON_VERSION}-armv7-musl.tar.gz -T copyfiles"
