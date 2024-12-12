7afe7e3b954d1f12f8ffde2a7a5ad7406383d034
Afonso Bordado <afonso@nortech.ai>
2024-12-12 17:30:42 +0000

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+PYTHON_VERSION=3.13
+IMAGE_NAME=arm32v7/python:${PYTHON_VERSION}-alpine
+
+docker run -it --rm -v "$(pwd):/work" -w /work ${IMAGE_NAME} /bin/sh -c "tar -cvf python-${PYTHON_VERSION}-armv7-musl.tar.gz -T copyfiles"
