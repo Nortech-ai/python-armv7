5f85b00dae2544731870374ab65ea01438d7fc19
Afonso Bordado <afonso@nortech.ai>
2024-12-16 11:55:46 +0000

    Add /usr/bin/python3 symlink

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 PYTHON_VERSION=3.13
 ALPINE_VERSION=3.21
 IMAGE_NAME=arm32v7/python:${PYTHON_VERSION}-alpine${ALPINE_VERSION}
+TARGZ_NAME=python-${PYTHON_VERSION}-alpine${ALPINE_VERSION}-armv7-musl.tar.gz
 
-docker run --rm --platform linux/arm/v7 -v "$(pwd):/work" -w /work ${IMAGE_NAME} /bin/sh -c "tar -cvf python-${PYTHON_VERSION}-alpine${ALPINE_VERSION}-armv7-musl.tar.gz -T copyfiles"
+docker run \
+  --rm \
+  --platform linux/arm/v7 \
+  -v "$(pwd):/work" \
+  -w /work \
+  ${IMAGE_NAME} \
+  /bin/sh -c "ln -s /usr/local/bin/python3 /usr/bin/python3 && tar -cvf ${TARGZ_NAME} -T copyfiles"
